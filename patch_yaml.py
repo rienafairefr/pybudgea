18b6d23e2a9a5cc9d57fe3e541e5aa199ca8c592
rienafairefr <rienafairefr@gmail.com>
2020-02-07 13:26:40 +0100

    remove the inline_response_ models

--- patch_yaml.py
+++ patch_yaml.py
@@ -22,26 +22,49 @@ def visit_tree(stack, func, r):
 with open('openapi.yaml', 'r') as openapi_yaml:
     openapi = yaml.load(openapi_yaml, Loader=yaml.SafeLoader)
 
-    remapped_get = {
-        '/connections': 'ConnectionsList',
-        '/account_types': 'AccountTypesList',
-        '/banks/{id_connector}/logos': 'ConnectorLogosList',
-        '/categories': 'CategoriesList',
-        '/users/{id_user}/accounts/{id_account}/operationsalert': 'UserAlertsList',
-        '/users/{id_user}/connections/{id_connection}/accounts/{id_account}/operationsalert': 'UserAlertsList',
-        '/users/{id_user}/operationsalert': 'UserAlertsList',
-    }
+    def remapped():
+        inline_schemas = []
+        for schema in openapi['components']['schemas']:
+            if schema.startswith('inline_response'):
+                inline_schemas.append('#/components/schemas/%s' % schema)
 
-    remap = {
-        openapi['paths'][p]['get']['responses'][200]['content']['application/json']['schema'][
-            '$ref']: '#/components/schemas/%s' % v
-        for p, v in remapped_get.items()
-    }
+        def inner_remapped(op):
+            for k, v in openapi['paths'].items():
+                ref = v.get(op, {}).get('responses', {}).get(200, {}).get('content', {}).get('application/json', {}).get(
+                    'schema', {}).get('$ref')
+                if ref and ref in inline_schemas:
+                    newRef = k.replace('/users/{id_user}', 'User')
+                    newRef = newRef.replace('/connections/{id_connection}', 'Connection')
+                    newRef = newRef.replace('/connectors/{id_connector}', 'Connector')
+                    newRef = newRef.replace('/accounts/{id_account}', 'Account')
+                    newRef = newRef.replace('/investments/{id_investment}', 'Account')
+                    newRef = newRef.replace('/transactionsclusters/{id_transactions_cluster}', 'TransactionsCluster')
+                    newRef = newRef.replace('/transactions/{id_transaction}', 'Transaction')
+                    newRef = newRef.replace('/recipients/{id_recipient}', 'Recipient')
+                    newRef = newRef.replace('/subscriptions/{id_subscription}', 'Subscription')
+                    newRef = newRef.replace('/banks/{id_connector}', 'BankConnector')
+                    newRef = newRef.replace('/webhooks/{id_webhook}', 'WebHook')
+                    newRef = newRef.replace('/providers/{id_connector}', 'Provider')
+                    while True:
+                        i = newRef.find('/')
+                        if i < 0:
+                            break
+                        newRef = newRef[:i] + newRef[i + 1:].capitalize()
+                    assert '{' not in newRef and '}' not in newRef
+                    yield k, newRef
+
+        for op in ('get', 'post'):
+            for p, v in inner_remapped(op):
+                yield openapi['paths'][p][op]['responses'][200]['content']['application/json']['schema'][
+                    '$ref'], '#/components/schemas/%s' % v
+
+    remap = dict(remapped())
 
     remapped_stacks = {
         tuple(k.split('/')[1:]): tuple(v.split('/')[1:]) for k, v in remap.items()
     }
 
+    assert len(set(remapped_stacks.values())) == len(remapped_stacks)
 
     def treat_node(stack, node):
         if 'parameters' in node:
