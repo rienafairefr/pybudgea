abb6676dd3067b7c4d90eb30945e18e3937356b3
rienafairefr <rienafairefr@gmail.com>
2020-03-11 15:56:26 +0100

    remove visit_tree stuff

--- patch_yaml.py
+++ patch_yaml.py
@@ -1,54 +1,52 @@
-import copy
-import json
 from pprint import pprint
 
 import jsonpatch
 import yaml
 
-from utils import parser, visit_tree
+from utils import parser
 
 
 def step_change(openapi):
     patch = []
 
+    def get_new_ref(path):
+        new_ref = path.replace('/users/{id_user}', 'User')
+        new_ref = new_ref.replace('/connections/{id_connection}', 'Connection')
+        new_ref = new_ref.replace('/connectors/{id_connector}', 'Connector')
+        new_ref = new_ref.replace('/accounts/{id_account}', 'Account')
+        new_ref = new_ref.replace('/investments/{id_investment}', 'Account')
+        new_ref = new_ref.replace('/transactionsclusters/{id_transactions_cluster}', 'TransactionsCluster')
+        new_ref = new_ref.replace('/transactions/{id_transaction}', 'Transaction')
+        new_ref = new_ref.replace('/recipients/{id_recipient}', 'Recipient')
+        new_ref = new_ref.replace('/subscriptions/{id_subscription}', 'Subscription')
+        new_ref = new_ref.replace('/banks/{id_connector}', 'BankConnector')
+        new_ref = new_ref.replace('/webhooks/{id_webhook}', 'WebHook')
+        new_ref = new_ref.replace('/providers/{id_connector}', 'Provider')
+        while True:
+            i = new_ref.find('/')
+            if i < 0:
+                break
+            new_ref = new_ref[:i] + new_ref[i + 1:].capitalize()
+        assert '{' not in new_ref and '}' not in new_ref
+        return new_ref
+
     def remapped():
-        inline_schemas = []
-        for schema in openapi['components']['schemas']:
-            if schema.startswith('inline_response'):
-                inline_schemas.append('#/components/schemas/%s' % schema)
-
-        def inner_remapped(op):
-            for k, v in openapi['paths'].items():
-                ref = v.get(op, {}).get('responses', {})\
-                    .get('200', {}).get('content', {}).get('application/json',{})\
+        for k, v in openapi['paths'].items():
+            for op in 'get', 'post':
+                ref = v.get(op, {}).get('responses', {}) \
+                    .get('200', {}).get('content', {}).get('application/json', {}) \
                     .get('schema', {}).get('$ref')
-                if ref and ref in inline_schemas:
-                    newRef = k.replace('/users/{id_user}', 'User')
-                    newRef = newRef.replace('/connections/{id_connection}', 'Connection')
-                    newRef = newRef.replace('/connectors/{id_connector}', 'Connector')
-                    newRef = newRef.replace('/accounts/{id_account}', 'Account')
-                    newRef = newRef.replace('/investments/{id_investment}', 'Account')
-                    newRef = newRef.replace('/transactionsclusters/{id_transactions_cluster}', 'TransactionsCluster')
-                    newRef = newRef.replace('/transactions/{id_transaction}', 'Transaction')
-                    newRef = newRef.replace('/recipients/{id_recipient}', 'Recipient')
-                    newRef = newRef.replace('/subscriptions/{id_subscription}', 'Subscription')
-                    newRef = newRef.replace('/banks/{id_connector}', 'BankConnector')
-                    newRef = newRef.replace('/webhooks/{id_webhook}', 'WebHook')
-                    newRef = newRef.replace('/providers/{id_connector}', 'Provider')
-                    while True:
-                        i = newRef.find('/')
-                        if i < 0:
-                            break
-                        newRef = newRef[:i] + newRef[i + 1:].capitalize()
-                    assert '{' not in newRef and '}' not in newRef
-                    yield k, newRef
 
-        for op in ('get', 'post'):
-            for p, v in inner_remapped(op):
-                yield openapi['paths'][p][op]['responses']['200']['content']['application/json']['schema'][
-                          '$ref'], '#/components/schemas/%s' % v
+                if ref and 'inline_response' in ref:
+                    new_ref = get_new_ref(k)
+                    yield openapi['paths'][k][op]['responses']['200']['content']['application/json']['schema'][
+                              '$ref'], '#/components/schemas/%s' % new_ref
 
-    remap = dict(remapped())
+    remap = {}
+    for k, v in remapped():
+        value = remap.get(k)
+        if value is None or len(value) > len(v):
+            remap[k] = v
 
     remapped_stacks = {
         tuple(k.split('/')[1:]): tuple(v.split('/')[1:]) for k, v in remap.items()
@@ -56,42 +54,37 @@ def step_change(openapi):
 
     assert len(set(remapped_stacks.values())) == len(remapped_stacks)
 
-    def treat_node(stack, key, node):
-        if len(stack) == 2 and stack[0] == 'paths':
-            if 'parameters' in node:
-                for idx, param in enumerate(node['parameters']):
-                    if param['name'] == 'expand':
-                        patch.append({
-                            "op": "add",
-                            "path": "/paths/%s/%s/parameters/%s/required" % (stack[1].replace('/', '~1'), key, idx),
-                            "value": False
-                        })
+    for url, item in openapi.get('paths').items():
+        safe_url = url.replace('/', '~1')
+        for method, node in item.items():
+            for idx, param in enumerate(node.get('parameters', [])):
+                if param['name'] == 'expand':
+                    patch.append({
+                        "op": "add",
+                        "path": "/paths/%s/%s/parameters/%i/required" % (safe_url, method, idx),
+                        "value": False
+                    })
             patch.append({
                 "op": "add",
-                "path": "/paths/%s/%s/security" % (stack[1].replace('/', '~1'), key),
+                "path": "/paths/%s/%s/security" % (safe_url, method),
                 "value": [{'Authorization': []}]
             })
-        if len(stack) == 2 and stack[0] == 'components' and stack[1] == 'schemas':
-            if 'example' in node:
+            schema_ref = node.get('responses', {}).get('200', {}).get('content', {})\
+                .get('application/json', {}).get('schema', {}).get('$ref')
+            remapped_ref = remap.get(schema_ref)
+            if schema_ref and remapped_ref:
                 patch.append({
-                    "op": "remove",
-                    "path": "/components/schemas/%s/example" % key
+                    "op": "add",
+                    "path": '/paths/%s/%s/responses/200/content/application~1json/schema/$ref' % (safe_url, method),
+                    "value": remapped_ref
                 })
-        remapped_ref = remap.get(node.get('$ref'))
-        if remapped_ref is not None:
-            new_stack = []
-            for s in stack:
-                new_stack.append(s.replace('/', '~1'))
-            new_stack = '/' + '/'.join(new_stack) + '/schema/$ref'
+
+    for name, item in openapi.get('components', {}).get('schemas', {}).items():
+        if 'example' in item:
             patch.append({
-                "op": "add",
-                "path": new_stack,
-                "value": remapped_ref
+                "op": "remove",
+                "path": "/components/schemas/%s/example" % name
             })
-
-        return node
-
-    visit_tree([], treat_node, openapi)
 
     for old, new in remapped_stacks.items():
         patch.append({
--- utils.py
+++ utils.py
@@ -11,18 +11,3 @@ NoDatesSafeLoader.yaml_implicit_resolvers = {
 parser = ArgumentParser()
 parser.add_argument('-i', dest='input')
 parser.add_argument('-o', dest='output')
-
-
-def nested_set(dic, keys, value):
-    for key in keys[:-1]:
-        dic = dic.setdefault(key, {})
-    dic[keys[-1]] = value
-
-
-def visit_tree(stack, func, r):
-    for k, v in r.items():
-        if isinstance(v, dict):
-            r[k] = func(stack, k, v)
-            stack.append(k)
-            visit_tree(stack, func, v)
-            stack.pop()
